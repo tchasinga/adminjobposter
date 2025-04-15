cc86a091bac015fcf3a4e414bf108830f63ff483
tchasinga <tchasingajacques@gmail.com>
2025-04-15 22:14:15 +0300

    feat: implement Input and Button components with styling and variant support

---
+++ app/Page/login/page.tsx
@@ -0,0 +1,14 @@
+"use client"
+import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button"
+
+
+
+
+import React from 'react'
+
+export default function Login() {
+  return (
+    <div>page</div>
+  )
+}
