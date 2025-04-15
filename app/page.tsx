f3d20a8d29773d5d40765547b4c09b84c8986fda
tchasinga <tchasingajacques@gmail.com>
2025-04-15 22:03:25 +0300

    new code is added now

--- app/page.tsx
+++ app/page.tsx
@@ -3,16 +3,7 @@ import { Button } from "./components/ui/button";
 
 export default function Home() {
   return (
-    <div className="">
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-      <Button variant="outline">Hello</Button>
-      <Button variant="default">Hello</Button>
-      <Button variant="destructive">Hello</Button>
-      <Button variant="secondary">Hello</Button>
-      <Button variant="ghost">Hello</Button>
-      <Button variant="link">Hello</Button>
+    <div className="">  
     </div>
   );
 }
