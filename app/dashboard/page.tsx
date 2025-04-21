3eeae538351a27ba3ffd45b6e3fa137b52faa2ab
tchasinga <tchasingajacques@gmail.com>
2025-04-21 18:53:19 +0300

    fix: remove unused eslint rule for react-hooks in Dashboard component

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-/* eslint-disable react-hooks/rules-of-hooks */
+ 
 "use client";
 import React, { useEffect } from 'react';
 import { useSelector } from 'react-redux';
